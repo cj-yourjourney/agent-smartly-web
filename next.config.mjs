01898ef99b972cca373b80889d8da385105d3c6d
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-10-08 10:45:36 -0700

    remove the tailing in settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Remove trailingSlash: true,
   images: {
     unoptimized: true // Required for static export
   }
