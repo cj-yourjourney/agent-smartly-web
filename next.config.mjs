5d6133a97e8b072c69036abdc17174238a52bad2
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-10-08 11:22:09 -0700

    added trailingSlash in the settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
-    unoptimized: true // Required for static export
+    unoptimized: true
   }
 }
 
