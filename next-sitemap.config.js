4c177c04749f824e8f7caf4e309bfeae61371f98
cj <49462194+cj-yourjourney@users.noreply.github.com>
2026-06-24 10:50:51 -0700

    added outDir

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   siteUrl: 'https://www.agentsmartly.com',
   generateRobotsTxt: true,
+  outDir: './out', // ← add this
   exclude: ['/auth/*', '/account', '/onboarding', '/learning/*'],
   robotsTxtOptions: {
     policies: [
