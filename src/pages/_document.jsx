a7187b220d6d6136452759b19e660328d7d78cde
cj <49462194+cj-yourjourney@users.noreply.github.com>
2026-06-24 10:19:18 -0700

    added Favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -12,6 +12,24 @@ export default function Document() {
 
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
 
         {/* Google Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
