7d271377a8d19715e551d7e62f96e7f4310854e7
cj <49462194+cj-yourjourney@users.noreply.github.com>
2026-07-22 16:37:58 -0700

    modified the posthog tag in app flie

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,6 @@
 // src/pages/_app.jsx
+import { useEffect } from 'react'
+import posthog from 'posthog-js'
 import { Provider } from 'react-redux'
 import { DefaultSeo } from 'next-seo'
 import { store } from '../shared/redux/store'
@@ -8,6 +10,19 @@ import Navbar from '../shared/components/Navbar'
 import '../sytles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && !posthog.__loaded) {
+      posthog.init('phc_RVwdiW3q1GG2KWzeAnxGuMFNPBbq53am6SgCZ5oYnJJ', {
+        api_host: 'https://us.i.posthog.com',
+        defaults: '2025-05-24',
+        person_profiles: 'identified_only',
+        loaded: (ph) => {
+          ph.register({ app: 'agentsmartly' })
+        }
+      })
+    }
+  }, [])
+
   return (
     <Provider store={store}>
       <DefaultSeo {...DEFAULT_SEO} />
